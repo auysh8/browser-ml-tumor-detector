b418488e36219316a1f8c7f45f0e8d26cdc55c40
auysh8 <auysh1993@gmail.com>
2026-07-13 13:49:06 +0530

    refactor: rename project and update configuration paths
    - Changed project name from "brain-tumor-detector" to "browser-ml-tumor-detector" in package.json.
    - Updated homepage URL in package.json to reflect the new project name.
    - Modified Vite configuration base path to match the new project name.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT: This line fixes the white screen!
-  base: "/brain-tumor-detector/", 
+  base: "/browser-ml-tumor-detector/",
   build: {
     chunkSizeWarningLimit: 1600,
-  }
-})
+  },
+});
